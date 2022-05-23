1be84b277297dd40a1cbd43dabe7018723f528b5
Myckol Fontes <myckol.ti@gmail.com>
2022-05-22 22:01:46 -0300

    criando component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { JogoDaVelhaModule } from './jogo-da-velha';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    JogoDaVelhaModule
+    AppComponent
   ],
   imports: [
     BrowserModule
