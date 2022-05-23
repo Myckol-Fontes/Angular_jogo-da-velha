812ffb6c93b5394bad4a32b31ae50c755c47a16e
Myckol Fontes <myckol.ti@gmail.com>
2022-05-22 22:13:48 -0300

    Implementando serviço novoJogo

--- src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
+++ src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
@@ -279,6 +279,16 @@ export class JogoDaVelhaService {
     return exibirVitoria;
   }
 
-
+  /**
+   * Inicializa um novo jogo, assim como exibe o tabuleiro.
+   *
+   * @return void
+   */
+  novoJogo(): void {
+    this.inicializar();
+    this._showFinal = false;
+    this._showInicio = false;
+    this._showTabuleiro = true;
+  }
 
 }
